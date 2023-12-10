bf2879531bdbb629407b39b6549f063b1b666d2f
Thalia Archibald <thalia@archibald.dev>
2023-12-10 05:31:40 -0800

    Update bindgen version

--- src-generated/update-bindings.sh
+++ src-generated/update-bindings.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+# cargo install bindgen-cli
 bindgen \
     --output bindings.rs \
-    --whitelist-function '^bitwuzla_(.*)$' \
-    --whitelist-type '^Bitwuzla(.*)$' \
-    --no-recursive-whitelist \
+    --allowlist-function '^bitwuzla_(.*)$' \
+    --allowlist-type '^Bitwuzla(.*)$' \
+    --no-recursive-allowlist \
     --no-doc-comments \
-    --raw-line 'use libc::{FILE, size_t};' \
+    --raw-line 'use libc::FILE;' \
     --no-prepend-enum-name \
     ../bitwuzla/src/api/c/bitwuzla.h
